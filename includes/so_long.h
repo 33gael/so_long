9012ac2e9c2ffb3b29e96eb75db6c8904e26cf08
Gael Ducasse <33gael.dev@gmail.com>
2025-12-15 12:49:11 +0100

    macrolibx + window opening

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,13 +6,14 @@
 /*   By: hazmat <hazmat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 00:43:45 by hazmat            #+#    #+#             */
-/*   Updated: 2025/12/14 00:44:53 by hazmat           ###   ########.fr       */
+/*   Updated: 2025/12/15 12:41:40 by hazmat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "../includes/libft/libft.h"
+# include "../libs/MacroLibX/includes/mlx.h"
+# include "../libs/libft/libft.h"
 
 #endif
