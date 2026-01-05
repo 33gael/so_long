9b9923c6de48e400fde05e1baa267b8358c580d9
Gael Ducasse <33gael.dev@gmail.com>
2026-01-05 11:34:41 +0100

    commit new start so_long

--- libs/get_next_line/get_next_line.h
+++ libs/get_next_line/get_next_line.h
@@ -1,13 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   window.c                                           :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hazmat <hazmat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/15 05:01:50 by hazmat            #+#    #+#             */
-/*   Updated: 2026/01/05 09:53:24 by hazmat           ###   ########.fr       */
+/*   Created: 2025/11/14 11:35:58 by hazmat            #+#    #+#             */
+/*   Updated: 2025/11/16 10:43:47 by hazmat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/so_long.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *src);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
