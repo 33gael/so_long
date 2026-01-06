980239cde7a154949d717cccb843d4b410f88075
Gael Ducasse <33gael.dev@gmail.com>
2026-01-06 13:55:11 +0100

    new init mlx

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -6,7 +6,7 @@
 /*   By: hazmat <hazmat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 12:45:15 by hazmat            #+#    #+#             */
-/*   Updated: 2026/01/05 12:48:18 by hazmat           ###   ########.fr       */
+/*   Updated: 2026/01/06 13:53:34 by hazmat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "../../includes/so_long.h"
 
-int	ft_main_utils(void);
+void	ft_start_game(t_data *data);
+int	ft_init_window(t_data *data);
 
 #endif
