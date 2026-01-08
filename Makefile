feb59159ef9ef8aa8185610e2cff446a781c6303
Gael Ducasse <33gael.dev@gmail.com>
2026-01-08 15:18:58 +0100

    final version no_lag

--- Makefile
+++ Makefile
@@ -32,6 +32,7 @@ SRCS            = $(SRCS_FOLDER)so_long.c \
 					$(SRCS_FOLDER)render_map.c \
 					$(SRCS_FOLDER)moves.c \
 					$(SRCS_FOLDER)map_check.c \
+					$(SRCS_FOLDER)map_path.c \
 
 # Transformation des chemins pour les objets [cite: 1]
 OBJS            = $(patsubst $(SRCS_FOLDER)%.c $(UTILS_FOLDER)%.c, $(OBJECTS_FOLDER)%.o, $(SRCS))
--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: hazmat <hazmat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 00:43:45 by hazmat            #+#    #+#             */
-/*   Updated: 2026/01/08 10:04:45 by hazmat           ###   ########.fr       */
+/*   Updated: 2026/01/08 14:16:26 by hazmat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,6 @@ void		ft_move_player(t_data *data, int dx, int dy);
 void		ft_destroy_images(t_data *data);
 void		ft_free_map(t_data *data);
 int			ft_check_map_validity(t_data *data);
+int			ft_check_path_validity(t_data *data);
 
 #endif
